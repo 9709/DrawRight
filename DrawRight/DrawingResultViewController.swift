8877746e192040eadca5911899f6585e99cd8ddc
Matthew Chan <mat_t4@hotmail.com>
2019-01-31 13:42:34 -0800

    Created PlayAreaViewController and DrawingResultViewController

---
+++ DrawRight/DrawingResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DrawingResultViewController.swift
+//  DrawRight
+//
+//  Created by Matthew Chan on 2019-01-31.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+import UIKit
+
+class DrawingResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DrawRight/PlayAreaViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlayAreaViewController.swift
+//  DrawRight
+//
+//  Created by Matthew Chan on 2019-01-31.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+import UIKit
+
+class PlayAreaViewController: UIViewController {
+
+}
