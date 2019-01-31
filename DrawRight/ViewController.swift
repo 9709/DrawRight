e2da0b43933440e4812ee86519390c0bd6130dc7
Matthew Chan <mat_t4@hotmail.com>
2019-01-30 21:09:58 -0800

    Initial Commit

---
+++ DrawRight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawRight
+//
+//  Created by Matthew Chan on 2019-01-30.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DrawRightTests/DrawRightTests.swift
@@ -0,0 +1,34 @@
+//
+//  DrawRightTests.swift
+//  DrawRightTests
+//
+//  Created by Matthew Chan on 2019-01-30.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+import XCTest
+@testable import DrawRight
+
+class DrawRightTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
