da4f1cf0fd6bc02973c14a7b684e1ea67b93f370
Matthew Chan <mat_t4@hotmail.com>
2019-01-31 19:24:16 -0800

    Creates BEGIN (UIImageView)
    - adding imageView
    - adding placeholder image in imageView
    - animate from right -> left

--- DrawRight/PlayAreaViewController.swift
+++ DrawRight/PlayAreaViewController.swift
@@ -9,5 +9,27 @@
 import UIKit
 
 class PlayAreaViewController: UIViewController {
-
+    @IBOutlet weak var beginButtonContainterView: UIView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func swipeGestureRecognizer(_ sender: UISwipeGestureRecognizer) {
+        
+        UIView.animate(withDuration: 1.0,
+                       animations: { () -> Void in
+                        self.beginButtonContainterView.bounds = CGRect(x: 1080, y: 0, width: self.beginButtonContainterView.bounds.width, height: self.beginButtonContainterView.bounds.height)
+                        },
+                       completion: { (animateComplete: Bool) -> Void in
+                        if animateComplete {
+                            self.beginButtonContainterView.removeFromSuperview()
+                        }
+                        })
+    }
+    
 }
+
