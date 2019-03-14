070989eef8d74ea83ecf21f90d07d49b4c234fbf
Matthew Chan <mat_t4@hotmail.com>
2019-03-13 19:29:59 -0700

    added screenshot of app

--- DrawRight/GenerateRandomSubjects.swift
+++ DrawRight/GenerateRandomSubjects.swift
@@ -15,6 +15,7 @@ struct GenerateRandomSubjects {
         let randomSubjects: [String] = ["Elephant", "Bus", "Guitar", "Microwave", "Car", "Skateboard", "Cup", "Running", "Rocket", "Plane", "Face", "Couch", "Pan", "Chair", "Microphone", "Foot", "Knife", "Book", "Fan", "House", "Keyboard", "Television", "Camera", "Cake", "Bulldozer", "Crane", "Shark", "Cell Phone", "Hat"]
         let subject = randomSubjects.randomElement()!
         
+        
         return subject
     }
 }
